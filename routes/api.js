0204605f10d3a9e01b2a2dd62fb0a7d9c688bd2b
einobaka <61556425+einobaka@users.noreply.github.com>
2020-09-10 21:12:06 -0700

    CRUD post route tested

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,15 @@
-// DB CRUD
+// CRUD API
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+// Get all workouts
 router.get('/workouts', (req, res) => {
   db.Workout.find({})
     // .sort({ date: -1 })
     .then(dbWorkout => {
       res.json({ 'Response': 'RES sent' })
+      res.send(console.log(dbWorkout));
       // res.json(dbWorkout);
     })
     .catch(err => {
@@ -15,20 +17,21 @@ router.get('/workouts', (req, res) => {
     });
 });
 
-
-// router.post("/api/workouts", ({ body }, res) => {
-//     Exercise.create(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
+router.post('/workouts', (req, res) => {
+  db.Workout.create({})
+    .then(dbWorkout => {
+      res.json({ 'Response': 'RES sent' })
+      // res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
 // router.post("/api/workouts/", ({ body }, res) => {
 //   Transaction.insertMany(body)
 //     .then(dbTransaction => {
+// res.json({ 'Response': 'RES sent' })
 //       res.json(dbTransaction);
 //     })
 //     .catch(err => {
